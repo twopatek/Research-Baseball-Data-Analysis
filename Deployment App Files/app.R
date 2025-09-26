74048721f38e3b7cf42b9191c86875c38557c59b
Matthew Adams <mada126@lsu.edu>
2025-09-25 19:31:28 -0500

    Reorganize repo

---
+++ Deployment App Files/app.R
@@ -0,0 +1,10 @@
+# Run the application
+app_dir <- file.path("Deployment App Files")
+
+source(file.path(app_dir, "global.R"), local = TRUE)
+source(file.path(app_dir, "ui.R"), local = TRUE)
+source(file.path(app_dir, "server.R"), local = TRUE)
+
+shinyApp(ui, server)
+
+# deployApp()
