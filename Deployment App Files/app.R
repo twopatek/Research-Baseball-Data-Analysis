84331f6df9e1d8b6121d8ef091689f1b8b7d169e
Matthew Adams <mada126@lsu.edu>
2025-10-07 12:56:11 -0500

    update app run file

--- Deployment App Files/app.R
+++ Deployment App Files/app.R
@@ -1,10 +1,9 @@
 # Run the application
 app_dir <- file.path("Deployment App Files")
 
+# Explicit sourcing
 source(file.path(app_dir, "global.R"), local = TRUE)
 source(file.path(app_dir, "ui.R"), local = TRUE)
 source(file.path(app_dir, "server.R"), local = TRUE)
 
 shinyApp(ui, server)
-
-# shiny::runApp("Deployment App Files")
